c326f4b71bac35224901ce0a24fb34ed6440a666
DennySPB <dennyspb@gmail.com>
2019-08-09 22:25:37 +0530

    cepheus: Added Lawnchair As Prebuilt
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- device.mk
+++ device.mk
@@ -50,3 +50,12 @@ PRODUCT_PACKAGES += \
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
+
+#Lawnchair
+PRODUCT_PACKAGES += \
+    Lawnchair \
+    LawnConf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
+    $(LOCAL_PATH)/prebuilts/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
