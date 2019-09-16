d4058d9116193f8fe3f46f97ab6c6f43e4e0f4b3
koen01 <koen.erens@gmail.com>
2019-09-16 19:58:39 +0200

    aex

--- aosp_cepheus.mk
+++ aosp_cepheus.mk
@@ -19,18 +19,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Syberia stuff.
-$(call inherit-product, vendor/syberia/common.mk)
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := syberia_cepheus
+PRODUCT_NAME := aosp_cepheus
 PRODUCT_DEVICE := cepheus
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9
 PRODUCT_MANUFACTURER := Xiaomi
-
-SYBERIA_BUILD_TYPE := OFFICIAL
 
 BUILD_FINGERPRINT := "Xiaomi/cepheus_eea/cepheus:9/PKQ1.181121.001/V10.2.15.0.PFAEUXM:user/release-keys"
 
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo syberia_cepheus-userdebug
+add_lunch_combo aosp_cepheus-userdebug
 export SKIP_ABI_CHECKS=true
