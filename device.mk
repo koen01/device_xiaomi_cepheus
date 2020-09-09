ae72069b5a331c4fd4d11484762d2cd95fa68159
koen01 <koen.erens@gmail.com>
2020-09-09 22:30:30 +0200

    some more fixes

--- device.mk
+++ device.mk
@@ -113,6 +113,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+TARGET_FACE_UNLOCK_SUPPORTED :=  TRUE
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
